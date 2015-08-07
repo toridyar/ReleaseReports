a96eee2d33d9edd8a8aa4b50375304c5969e86d5
toridyar <toridyar319@hotmail.com>
2015-08-07 18:54:31 -0400

    Updating business logic

--- controllers.js
+++ controllers.js
@@ -3,17 +3,16 @@ var releaseReportsControllers = angular.module('releaseReportsControllers',[]);
 
 releaseReportsControllers.controller('searchCtrl',['$scope','$http',
   function($scope, $http){
-
+    var path = "/release/jiradata"
     $http.defaults.headers.get={'Content-Type':'application/json'};
-    $http.get("http://devopsapi.ignitionone.com/release/jiradata").success(function(data){
+    $http.get("http://localhost:3000/api/devopsapi.ignitionone.com/"+encodeURIComponent(path)).success(function(data){
      $scope.devReleaseResults=data;
      });
 
-    var path = "/rest/api/2/search";
-    var parameters = "?jql=project in (MEDIACORE,DISPLAY,DISPLAYTWO,DISPLAYDA,PROFILE,SEARCHDEV) AND issuetype = Bug AND status != Closed AND created >= '2015/07/22' AND created <= '2015/08/03'&fields=issuekey,priority,summary,reporter,status,issuelinks";
+    var path = "/rest/api/2/search?jql=project in (DMSDEV) AND issuetype in subTaskIssueTypes() AND created >= '2015/07/22' AND created <= '2015/08/03'&fields=issuekey,priority,summary,reporter,status,issuelinks";
 
     $http.defaults.headers.get={'Content-Type':'application/json'};
-    $http.get("http://localhost:3000/jira/"+encodeURIComponent(path)+parameters).success(function(data){
+    $http.get("http://localhost:3000/jira/"+encodeURIComponent(path)).success(function(data){
      $scope.searchResults=data.issues;
      });
 
